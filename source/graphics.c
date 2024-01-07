4bdf5ae66c2ccdaecc3df1736fa9386580e0de3c
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-01-07 13:11:34 +0100

    Add charmander sprite

--- source/graphics.c
+++ source/graphics.c
@@ -1,4 +1,5 @@
 #include "graphics.h"
+#include "007.h"
 
 const unsigned char empty_tile[] = {
     0,0,0,0,0,0,0,0,
@@ -21,6 +22,7 @@ const unsigned char full_tile[] = {
     1,1,1,1,1,1,1,1,
 };
 
+u16* gfx;
 
 void init_red_background(){
 
@@ -45,16 +47,50 @@ void init_red_background(){
     }
 }
 
-// init_dino_sprite(){
-//     VRAM_B_CR = VRAM_ENABLE | VRAM_B_MAIN_SPRITE_0x06400000;
-//     oamInit(&oamMain, SpriteMapping_1D_32, false);
-//     gfx = oamAllocateGfx(&oamMain, SpriteSize_64x64, SpriteColorFormat_256Color);
-//     swiCopy(raptor)
-// }
+void init_dino_sprite(){
+    VRAM_B_CR = VRAM_ENABLE | VRAM_B_MAIN_SPRITE_0x06400000;
+    oamInit(&oamMain, SpriteMapping_1D_32, false);
+    gfx = oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+    swiCopy(_07Pal, SPRITE_PALETTE, _07PalLen/2);
+    swiCopy(_07Tiles, gfx, _07TilesLen/2);
+
+    int x = 0, y = 0, keys;
+
+    while(1){
+    	//Read held keys
+    	scanKeys();
+    	keys = keysHeld();
+
+    	//Modify position of the sprite accordingly
+    	if((keys & KEY_RIGHT) && (x < (SCREEN_WIDTH - SPRITE_WIDTH))) x+=2;
+    	if((keys & KEY_DOWN) && (y < (SCREEN_HEIGHT - SPRITE_HEIGHT))) y+=2;
+    	if((keys & KEY_LEFT) && (x  > 0)) x-=2;
+    	if((keys & KEY_UP) && (y  > 0)) y-=2;
+
+    	oamSet(&oamMain, 	// oam handler
+    		0,				// Number of sprite
+    		x, y,			// Coordinates
+    		0,				// Priority
+    		0,				// Palette to use
+    		SpriteSize_32x32,			// Sprite size
+    		SpriteColorFormat_256Color,	// Color format
+    		gfx,			// Loaded graphic to display
+    		-1,				// Affine rotation to use (-1 none)
+    		false,			// Double size if rotating
+    		false,			// Hide this sprite
+    		false, false,	// Horizontal or vertical flip
+    		false			// Mosaic
+    		);
+    	swiWaitForVBlank();
+    	//Update the sprites
+		oamUpdate(&oamMain);
+    }
+}
 
 void init_graphics(){
 
     init_red_background();
+    init_dino_sprite();
 }
 
 
--- source/graphics.h
+++ source/graphics.h
@@ -4,3 +4,10 @@
 #define DARK_RED ARGB16(1, 5, 0, 0)
 
 void init_graphics(void);
+
+
+#define SCREEN_WIDTH	256
+#define	SCREEN_HEIGHT	192
+
+#define	SPRITE_WIDTH	32
+#define	SPRITE_HEIGHT	32
