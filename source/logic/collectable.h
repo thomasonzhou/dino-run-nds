3f6a876d228991d152222d2f797d4ee8d8a9aa61
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2024-01-08 16:28:17 +0100

    Add override

--- source/logic/collectable.h
+++ source/logic/collectable.h
@@ -1,5 +1,9 @@
 #include "collidable.h"
 
 class Collectable: public Collidable{
-    
+public:
+    Collectable(int x1, int y1, int x2, int y2): Collidable(x1, y1, x2, y2){};
+    void handle_collision() override{
+        return;
+    }
 };
--- source/logic/collidable.h
+++ source/logic/collidable.h
@@ -1,6 +1,10 @@
 class Collidable{
+protected:
     int x1;
     int y1;
     int x2;
     int y2;
+public:
+    Collidable(int x1, int y1, int x2, int y2) : x1(x1), y1(y1), x2(x2), y2(y2) {};
+    virtual void handle_collision();
 };
